bc5ea5c373060a3d0bb14edd965f87415f9c7878
eff <frxpoulin@gmail.com>
2015-04-23 15:25:32 -0400

    better style

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ hljs.configure({tabReplace: '    '});
 hljs.initHighlighting()
 
 var code_samples = {
-	cs : 'using Invoked\n\ntry {\n    var add = new Invokable<int>("github/mentum/invoked-samples", "php-addition");\n    var result = await add.CallAsync(5, 10);\n    Console.WriteLine("Success: {0}", result); // Prints 15\n}\ncatch(Exception e) {\n    Console.WriteLine("Error: {0}", e.Message);\n}',
+	cs : 'using Invoked\n\ntry {\n    var cloudPhpAdd = new Invokable<int>("github/mentum/invoked-samples", "php-addition");\n    var result = await add.CallAsync(5, 10);\n    Console.WriteLine("Success: {0}", result); // Prints 15\n}\ncatch(Exception e) {\n    Console.WriteLine("Error: {0}", e.Message);\n}',
 	python : "import invoked\n\nwith invoked('invoked-example', 'javascript-addition') as cloud_js_add:\n\n    print cloud_js_add(2,3)     # prints 5",
-	go : 'import (\n    "fmt"\n    "github.com/mentum/invoked"\n)\n\nremote_csharp_add := invoked("github/mentum/invoked-samples", "csharp-addition")\n\nres := remote_csharp_add(8, 7)\nfmt.Println(<-res)    // prints 15',
+	go : 'import (\n    "fmt"\n    "github.com/mentum/invoked"\n)\n\nCloudCsharpAdd := invoked("github/mentum/invoked-samples", "csharp-addition")\n\nres := CloudCsharpAdd(8, 7)\nfmt.Println(<-res)    // prints 15',
 	ruby : "class Greeter\n  def salute\n    puts 'Hello #{@name}!'\n  end\nend\n\ng = Greeter.new('world')\ng.salute",
-	js : "var invoked = require('invoked');\n\nvar add = invoked('github/mentum/invoked-samples', 'csharp-addition');\nadd(5, 10, function(err, result) {\n    if (err) console.log('Error: ', err);\n    else     console.log('Success: ', result); // Prints 15\n});",
+	js : "var invoked = require('invoked');\n\nvar cloudCsharpAdd = invoked('github/mentum/invoked-samples', 'csharp-addition');\ncloudCsharpAdd(5, 10, function(err, result) {\n    if (err) console.log('Error: ', err);\n    else     console.log('Success: ', result); // Prints 15\n});",
 	sh: "$ curl -X POST https://api.invoked.io/github/mentum/invoked-samples/csharp-addition/invoke \ \n    -H 'Content-Type: application/json'\ \n    -u 'api:<your api key here>'\ \n    -d '{\"a\":5,\"b\":\"10\"}' "
 }
 
