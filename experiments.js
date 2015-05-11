8de421b5702d0be45edc7e50b0ef5ffb31ced99a
eff <frxpoulin@gmail.com>
2015-05-11 16:06:50 -0400

    change the page a bit for more simplicity

--- experiments.js
+++ experiments.js
@@ -1,7 +1,3 @@
-$('#register').click(function(){
-    analytics.track('Clicked on get started');
-});
-
 $('#pay-button').click(function(){
     analytics.track('Clicked on pay 1$ from header');
 });
@@ -12,6 +8,10 @@ $('#pay-button-2').click(function(){
 
 $('.select-langage').click(function(event){
     analytics.track('Clicked on langage ' + $(event.target).data('langage'));
+});
+
+$('.learn-more').click(function(){
+    analytics.track('Clicked on learn more ' + $(this)[0].id);
 });
 
 analytics.trackLink($('#tweet'), 'Clicked on tweet from header');
